13d82d0b3fa3daadd6807dfac9272a3b4d616a3d
showrav-BS1334 <showrav.paul@brainstation-23.com>
2023-04-12 13:34:46 +0600

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key] // primary key
+        public int Id { get; set; }
+        [Required] // can not be null
+        public String Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be between 1 to 100!")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
